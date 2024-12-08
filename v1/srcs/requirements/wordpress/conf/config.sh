06c43e0cded1df1b60dd582e2fbaaf2eb7284074
Kate <100698139+k8sansom@users.noreply.github.com>
2024-12-08 10:50:35 +0000

    working with a makefile

--- v1/srcs/requirements/wordpress/conf/config.sh
+++ v1/srcs/requirements/wordpress/conf/config.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-cd /var/www/wordpress
-wp core config	--dbhost=$DB_HOST \
-				--dbname=$DB_NAME \
-				--dbuser=$DB_USER \
-				--dbpass=$DB_PASSWORD \
-				--allow-root
+cd /var/www/html
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+chmod +x wp-cli.phar
+./wp-cli.phar core download --allow-root
+./wp-cli.phar config create --dbname=wordpress --dbuser=wpuser --dbpass=password --dbhost=mariadb --allow-root
+./wp-cli.phar core install --url=localhost --title=inception --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
 
-wp core install --title=$WP_TITLE \
-				--admin_user=$WP_ADMIN_USER \
-				--admin_password=$WP_ADMIN_PASSWORD \
-				--admin_email=$WP_ADMIN_MAIL \
-				--url=$WP_URL \
-				--allow-root
-
-wp user create $WP_USER $WP_USER_MAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
-cd -
-
-# run php-fpm7.3 listening for CGI request
-# php-fpm7.4 -F
+php-fpm7.4 -F
