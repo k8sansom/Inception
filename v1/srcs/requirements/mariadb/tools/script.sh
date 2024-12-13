a0e57ea09de0be895e24dc7c6270146020b81f42
Kathryn Sansom <ksansom@c3a8c1.42berlin.de>
2024-12-13 10:39:40 +0100

    fixed mariadb script

--- v1/srcs/requirements/mariadb/tools/script.sh
+++ v1/srcs/requirements/mariadb/tools/script.sh
@@ -17,13 +17,13 @@ echo "Creating database"
 mariadb -u root -p$(<"/run/secrets/mariadb_root_pass") -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
 
 echo "Creating user"
-mariadb -u root -p$(<"/run/secrets/mariadb_root_pass") -e "CREATE USER IF NOT EXISTS \`$(<"/run/secrets/db_user")\`@'localhost' IDENTIFIED BY '$(<"/run/secrets/db_password")';"
+mariadb -u root -p$(<"/run/secrets/mariadb_root_pass") -e "CREATE USER IF NOT EXISTS \`$(<"/run/secrets/mariadb_user")\`@'localhost' IDENTIFIED BY '$(<"/run/secrets/mariadb_pass")';"
 
 echo "Granting privileges"
-mariadb -u root -p$(<"/run/secrets/mariadb_root_pass") -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`$(<"/run/secrets/db_user")\`@'%' IDENTIFIED BY '$(<"/run/secrets/db_password")';"
+mariadb -u root -p$(<"/run/secrets/mariadb_root_pass") -e "GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO \`$(<"/run/secrets/mariadb_user")\`@'%' IDENTIFIED BY '$(<"/run/secrets/mariadb_pass")';"
 
 echo "Setting root password"
-mariadb -u root -p$(<"/run/secrets/mariadb_root_pass") -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$(<"/run/secrets/db_root_password")';"
+mariadb -u root -p$(<"/run/secrets/mariadb_root_pass") -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$(<"/run/secrets/mariadb_root_pass")';"
 
 echo "Flushing privileges"
 mariadb -u root -p$(<"/run/secrets/mariadb_root_pass") -e "FLUSH PRIVILEGES;"
